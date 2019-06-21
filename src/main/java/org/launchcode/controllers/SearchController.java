cd3e75b6b8b388e669a080fce3e4838cef13c063
Isaac Arcos <isaac.arcos696@gmail.com>
2019-06-20 21:06:15 -0500

    Completed TechJobs-MVC required assignments ;

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -22,6 +23,31 @@ public class SearchController {
         return "search";
     }
 
+    @RequestMapping(value="results")
+    public String searchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        ArrayList<HashMap<String, String>> filteredJobs = JobData.findByColumnAndValue(searchType, searchTerm);
+//
+//        model.addAttribute("columns", ListController.columnChoices);
+//        model.addAttribute("jobs", jobs);
+//        model.addAttribute("searchType", searchType);
+//
+//        return "search";
+//
+//    }
+
+        if (searchType.equals("all")){
+            filteredJobs = JobData.findByValue(searchTerm);
+        } else {
+            filteredJobs = JobData.findByColumnAndValue(searchType,searchTerm);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobPosts",filteredJobs);
+        model.addAttribute("searchType", searchType);
+
+        return "search";
+    }
     // TODO #1 - Create handler to process search request and display results
 
 }
